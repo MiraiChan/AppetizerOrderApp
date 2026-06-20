a4fb76ad2a1ea9177346560b958e57a6ac0a5c2e
Almira Khafizova <mimirka80@gmail.com>
2026-06-21 01:12:44 +0200

    Initial Commit

---
+++ AppetizerOrder/AppetizerOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerOrderApp.swift
+//  AppetizerOrder
+//
+//  Created by Almira Khafizova on 21.06.26.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

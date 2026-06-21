a5cf063add5ff8bce7f4045669645e715b2162f1
Almira Khafizova <mimirka80@gmail.com>
2026-06-21 14:39:07 +0200

    Refactor: separate HStack into a AppetizerListCell

---
+++ AppetizerOrder/AppetizerListCell.swift
@@ -0,0 +1,31 @@
+//
+//  AppetizerListCell.swift
+//  AppetizerOrder
+//
+//  Created by Almira Khafizova on 21.06.26.
+//
+import SwiftUI
+
+struct AppetizerListCell: View {
+  let appetizer: Appetizer
+  
+  var body: some View {
+    HStack {
+      Image("asian-flank-steak")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 120, height: 90)
+        .cornerRadius(8)
+      
+      VStack(alignment: .leading, spacing: 5) {
+        Text(appetizer.name)
+          .font(.title2)
+          .fontWeight(.medium)
+        Text("$\(appetizer.price, specifier: "%.2f")")
+          .foregroundStyle(.secondary)
+          .fontWeight(.semibold)
+      }
+      .padding(.leading)
+    }
+  }
+}

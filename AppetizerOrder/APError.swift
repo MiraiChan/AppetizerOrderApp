999a95a734b720aeab5f579436ade4afa3587487
Almira Khafizova <mimirka80@gmail.com>
2026-06-22 14:46:39 +0200

    Feat: add NetworkManager and Error handling

---
+++ AppetizerOrder/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  AppetizerOrder
+//
+//  Created by Almira Khafizova on 21.06.26.
+//
+
+import Foundation
+
+enum APError: Error {
+  case invalidURL
+  case invalidResponse
+  case invalidData
+  case unableToComplete
+}

502f5b850a94246736f43305f7c787d654dea19e
Almira Khafizova <mimirka80@gmail.com>
2026-06-24 00:05:21 +0200

    Feat: structure project

--- AppetizerOrder/Views/Cells/AppetizerListCell.swift
+++ AppetizerOrder/Views/Cells/AppetizerListCell.swift
@@ -4,22 +4,36 @@
 //
 //  Created by Almira Khafizova on 21.06.26.
 //
+
 import SwiftUI
 
 struct AppetizerListCell: View {
+  
   let appetizer: Appetizer
   
   var body: some View {
     HStack {
-      AppetizerRemoteImage(urlString: appetizer.imageURL)
-        .aspectRatio(contentMode: .fit)
-        .frame(width: 120, height: 90)
-        .cornerRadius(8)
+      
+      AsyncImage(url: URL(string: appetizer.imageURL)) { image in
+        image
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(width: 120, height: 90)
+          .cornerRadius(8)
+      } placeholder: {
+        Image("food-placeholder")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(width: 120, height: 90)
+          .cornerRadius(8)
+      }
+      
       
       VStack(alignment: .leading, spacing: 5) {
         Text(appetizer.name)
           .font(.title2)
           .fontWeight(.medium)
+        
         Text("$\(appetizer.price, specifier: "%.2f")")
           .foregroundStyle(.secondary)
           .fontWeight(.semibold)
